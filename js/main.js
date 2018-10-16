4608d9c8a0bca094de9252832b9cfcf23842a1e2
rextao <87639243@qq.com>
2018-10-17 00:14:39 +0800

    main函数功能顺序调整

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ const { Parser } = parse;
 const parseData = Parser.init();
 // data为解析了账本后数据,实际的csv数据
 parseData.then((data) => {
-  // *********获取文件夹名，移动文件****************************
-  const process = new Process();
-  process.init(data);
   // ******************账单比较*******************
   const compare = new Compare(data);
   // 进行了账单比较，
   compare.init();
+  // *********获取文件夹名，移动文件****************************
+  const process = new Process();
+  process.init(data);
   // 将解析后的数据写入billparse目录,process.billname为写入文件的文件名
-  Process.writeAlltoBillParse(compare.parseallstr, process.billname);
+  Process.writeAlltoBillParse(compare.compareallstr, process.billname);
 }).catch(err => console.log(err));
