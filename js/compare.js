21180e7ba46d6eda52a9c579abdc152556f816eb
rextao <87639243@qq.com>
2018-11-15 15:48:53 +0800

    增加注释

--- js/compare.js
+++ js/compare.js
@@ -69,11 +69,13 @@ class Compare {
       }
     }
     this.setCompareAllStr(result, alipayRes, ...others);
+    this.setCompared(result);
     console.log('**************解析结果：******************');
     Compare.consoleResultTable(consoleArr, result, alipayRes, others);
     console.table(consoleArr);
   }
 
+  // 循环其他账单，分别与alipay账单的每一行（item）比较，获得结果
   static loopOthers(item, others) {
     const result = [];
     const alipayDate = item[0];
@@ -94,48 +96,6 @@ class Compare {
       }
     }
     return result;
-  }
-
-  // cgb与alipay对比
-  cgbWithAlipay(cgb = [], alipay) {
-    // cgb有两行无用信息，非数据
-    // alipay，生成未成功解析账单是删除alipay成功解析的行
-    console.log(`原始账单：cgb为：${cgb.length - 2}，alipay为：${alipay.length}`);
-    const resultcgb = [];
-    const result = [];
-    // cgb账单第一行都为自定义信息，第二行为描述信息，即账单title
-    for (let i = 2; i < cgb.length; i += 1) {
-      // 判断数据是否匹配上；
-      let ismatch = false;
-      const cgbitem = cgb[i];
-      const cgbtime = cgbitem[0];// 时间
-      const cbgprice = cgbitem[1];// 价格
-      // alipay没有title的描述信息，第一行为自定义信息
-      for (let j = 1; j < alipay.length; j += 1) {
-        const alipayitem = alipay[j];
-        const alipaytime = alipayitem[0];// 时间
-        const alipayprice = alipayitem[1];// 价格
-        // 两个账单时间递减排列,如cgbtime在aplipaytime之后，则表示cgb账单肯定没有匹配的了
-        if (date.isAfter(cgbtime, alipaytime)) {
-          break;
-        }
-        // 如账单时间相同，价格相同
-        if (date.isSame(cgbtime, alipaytime) && cbgprice === alipayprice) {
-          cgbitem.push(...alipayitem.splice(2));
-          result.push(cgbitem);
-          alipay.splice(j, 1);
-          ismatch = true;
-          break;
-        }
-      }
-      // alipay循环完，发现ismatch为false，表示当前i的item未匹配上
-      if (!ismatch) {
-        resultcgb.push(cgbitem);
-      }
-    }
-    console.log(`成功解析：${result.length},cgb剩余：${resultcgb.length},alipay剩余：${alipay.length}`);
-    this.setCompared(result);
-    this.setCompareAllStr(result, resultcgb, alipay);
   }
 
   getCompared() {
